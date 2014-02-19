861abda60a514f1ffd0ab7ef512c41b54169cdd9
SHIVA73 <shiva@coresonant.com>
2014-02-19 15:39:10 +0530

    Revert "CODE"
    This reverts commit 42734f1b06acfeb2ba27d15dbd7e3e38a7d824fd.

--- New Project/product/timeandattendance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from timeandattendance.models import *
-
-admin.site.register(shift_type)
-admin.site.register(workday_schedule)
-admin.site.register(shifttype_schedule_map)
-admin.site.register(person_workday_schedule_map)
-
